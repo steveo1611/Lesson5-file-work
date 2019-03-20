d4e0111689d778ef4e06a4c9a128865babd5de27
Steve Oldfield <steveold1611@gmail.com>
2019-03-20 11:09:28 -0600

    Add project files.

---
+++ Lesson5-file-work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5_file_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
