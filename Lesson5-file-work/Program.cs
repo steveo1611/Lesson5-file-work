8fd9a17858f5bf72d3e31a255628890039322824
Steve Oldfield <steveold1611@gmail.com>
2019-03-20 12:48:34 -0600

    Initial repo creation w/file

--- Lesson5-file-work/Program.cs
+++ Lesson5-file-work/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Program to retrieve the following data:
+// Replace the IP address of the device for which hostname is 'HP2D0DB0' to '15.13.19.22'
+// print the updated value
+// Get the list of the device's ocvStatus and verify if it contains 'GenuineHP'
+// this data will be extracted from xml file in the files folder.
+
+using System;
 
 namespace Lesson5_file_work
 {
@@ -6,7 +12,7 @@ namespace Lesson5_file_work
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Heaven!");
         }
     }
 }
