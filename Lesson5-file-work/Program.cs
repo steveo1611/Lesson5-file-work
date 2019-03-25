11f3f679c13f4b10df30c54ad53164ac10b55d92
Steven Oldfield <steveold1611@gmail.com>
2019-03-25 06:21:57 -0600

    not in good shape

--- Lesson5-file-work/Program.cs
+++ Lesson5-file-work/Program.cs
@@ -2,9 +2,12 @@
 // Replace the IP address of the device for which hostname is 'HP2D0DB0' to '15.13.19.22'
 // print the updated value
 // Get the list of the device's ocvStatus and verify if it contains 'GenuineHP'
-// this data will be extracted from xml file in the files folder.
+// this data will be extracted from json file in the files folder.
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Lesson5_file_work
@@ -14,27 +17,88 @@ namespace Lesson5_file_work
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Heaven!");
-            JFile();
+            JsonFile();
+       //     RdFile();
         }
-    }
+        /*        public static void RdFile()
+                    {
+                    string filePath1 = "files/lesson-W5.json";
+                    string filePath2 = "files/lesson-w5-updated.json";
+                    //           string[] readFile = new string[jobj];
+                    string readFile = "";
+                    string writeFile = "";
+          //          using (StreamReader r = new StreamReader(filePath1))
+                    using (StreamReader r = File.OpenText(filePath1))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        Printers printer = (Printers)serializer.Deserialize(r);
+           //             Printers printer = (Printers)serializer.Deserialize(r, typeof(Printers));
 
-    public class JFile
-    {
-        using (StreamReader r = new StreamReader("./files/lesson-W5.json"))
+                        var line = r.ReadToEnd();
+                  //      dynamic jobj = JObject.Parse(line);
+                        dynamic jobj = JObject.FromObject(line);
+                        readFile = jobj.ToString();
+        //                List<Printers> printer = JsonConvert.DeserializeObject<List<Printers>> (jobj);
+        //               List<Printers> printer = new List<Printers>(jobj);
+
+
+                        //               var jobj = JObject.Parse(line);
+                        //             readFile = jobj.ToString();
+                        //               foreach (var item in line)
+                        Console.WriteLine(jobj);
+                        {
+                            //                   if (item == )
+
+                            foreach (var item in printer)
+                            {
+                                Console.WriteLine(item);
+                                Console.ReadKey();
+                            }
+                            }
+
+                    }
+                    }
+             public class Printers
+                {
+                    public int localDeviceId { get; set; }
+                    public string hostName { get; set; }
+                    public string ipAddress { get; set; }
+                    public string modelName { get; set; }
+                    public string serialNumber { get; set; }
+                    public string ocvStatus { get; set; }
+                }
+
+
+
+                }
+            }
+        */
+
+
+        public static object JsonFile()
         {
-    
+            using (StreamReader r = new StreamReader("./files/lesson-W5.json"))
+            {
+                string line = r.ReadToEnd();
+             //   object readFile = "";
+                Items item = new Items();
+                //List<Items> item = JsonConvert.DeserializeObject<List<Items>>(line);
+                JsonConvert.PopulateObject(line, item);
+                return item;
+            }
         }
-    }
 
-    public class Item
-    {
-        public int localDeviceId;
-        public string hostName;
-        public string ipAddress;
-        public string modelName;
-        public string serialNumber;
-        public string ocvStatus;
-    }
+        public class Items
+        {
+            public int localDeviceId { get; set; }
+            public string hostName { get; set; }
+            public string ipAddress { get; set; }
+            public string modelName { get; set; }
+            public string serialNumber { get; set; }
+            public string ocvStatus { get; set; }
+        }
 
+
+    }
 }
  
