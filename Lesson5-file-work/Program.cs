7337b19810afa9f27924ae939483106fdb681d7e
Steve Oldfield <steveold1611@gmail.com>
2019-03-26 15:47:12 -0600

    able to extract hostname and change IP now onto the ocvstatus query....

--- Lesson5-file-work/Program.cs
+++ Lesson5-file-work/Program.cs
@@ -18,50 +18,57 @@ namespace Lesson5_file_work
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Heaven!");
-            //JsonFile();
             RdFile();
         }
 
         public static void RdFile()
         {
             string filePath1 = "files/lesson-W5.json";
-           // string filePath1 = "D:\\projects\\source\\file\\lesson-W5.json";
+            // string filePath1 = "D:\\projects\\source\\file\\lesson-W5.json";
             string filePath2 = "D:\\projects\\source\\file\\lesson-w5-updated.json";
 
             DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(File.ReadAllText(filePath1));
             DataTable dataTable = dataSet.Tables["results"];
-            Console.WriteLine(dataTable);
 
             foreach (DataRow row in dataTable.Rows)
             {
-                Console.WriteLine(row["localDeviceId"]);
-                Console.ReadKey();
+                object hn = row["hostName"];
+                string file = (string)hn;
 
+                if (file.Contains("HP2D0DB0"))
+                {
+                    // If true change IP for this object to '15.13.19.22'
+                    Console.WriteLine(row["ipAddress"]);
+                    row["ipAddress"] = "15.13.19.22";
+                    Console.WriteLine(row["ipAddress"]);
+                    // loop through object to display?? or use foreach?  for (i > 0;row.len i++; )
+                }
+                else
+                {
+                    Console.WriteLine("Losser");
+                }
+                foreach (DataRow row2 in dataTable.Rows)
+                {
+                    // Get the list of the device's ocvStatus and verify if it contains 'GenuineHP'
+                    object ocvStat = row2["ocvStatus"];
+                    string ovc = (string)ocvStat;
+
+
+
+                    if (ovc.Contains("GenuineHP"))
+                    {
+                        Console.WriteLine("WINNER");
+                    }
+
+                    Console.WriteLine("Losser");
+                    Console.ReadKey();
+
+                }
             }
         }
     }
 }
 
-  /*          using (StreamReader file = File.OpenText(@"D:\projects\source\file\lesson-W5.json"))
-            {
-                JsonSerializer sr = new JsonSerializer();
-                Printers printer2 = (Printers)sr.Deserialize(file, typeof(Printers));
-                Console.WriteLine(printer2);
-            }
-                    }
-        public class Printers
-                    {
-                    public int localDeviceId { get; set; }
-                    public string hostName { get; set; }
-                    public string ipAddress { get; set; }
-                    public string modelName { get; set; }
-                    public string serialNumber { get; set; }
-                    public string ocvStatus { get; set; }
-                    }
-
-    }
-}
-*/
 /*  Deserialize JSON =======================================
 
 string json = @"{
