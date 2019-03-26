ed1e50f7492333bebacfad55ae76529f1e2fc0ff
Steven Oldfield <steveold1611@gmail.com>
2019-03-25 21:09:20 -0600

    Pulling in the json object now...on to next item.

--- Lesson5-file-work/Program.cs
+++ Lesson5-file-work/Program.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 
 namespace Lesson5_file_work
@@ -22,15 +23,31 @@ namespace Lesson5_file_work
         }
 
         public static void RdFile()
-                    {
-                    string filePath1 = "files/lesson-W5.json";
-                    string filePath2 = "files/lesson-w5-updated.json";
-                    string readFile = "";
-                    string writeFile = "";
-            Printers print = new 
+        {
+            string filePath1 = "files/lesson-W5.json";
+           // string filePath1 = "D:\\projects\\source\\file\\lesson-W5.json";
+            string filePath2 = "D:\\projects\\source\\file\\lesson-w5-updated.json";
 
+            DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(File.ReadAllText(filePath1));
+            DataTable dataTable = dataSet.Tables["results"];
+            Console.WriteLine(dataTable);
 
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["localDeviceId"]);
+                Console.ReadKey();
 
+            }
+        }
+    }
+}
+
+  /*          using (StreamReader file = File.OpenText(@"D:\projects\source\file\lesson-W5.json"))
+            {
+                JsonSerializer sr = new JsonSerializer();
+                Printers printer2 = (Printers)sr.Deserialize(file, typeof(Printers));
+                Console.WriteLine(printer2);
+            }
                     }
         public class Printers
                     {
@@ -44,6 +61,39 @@ namespace Lesson5_file_work
 
     }
 }
-        
+*/
+/*  Deserialize JSON =======================================
 
- 
+string json = @"{
+  'Name': 'Bad Boys',
+  'ReleaseDate': '1995-4-7T00:00:00',
+  'Genres': [
+    'Action',
+    'Comedy'
+  ]
+}";
+
+Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+string name = m.Name;
+// Bad Boys
+
+    */
+
+ /* Linq to JSON.....==================================
+JArray array = new JArray();
+array.Add("Manual text");
+array.Add(new DateTime(2000, 5, 23));
+
+JObject o = new JObject();
+o["MyArray"] = array;
+
+string json = o.ToString();
+// {
+//   "MyArray": [
+//     "Manual text",
+//     "2000-05-23T00:00:00"
+//   ]
+// }
+
+    */
