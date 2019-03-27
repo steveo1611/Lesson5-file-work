12e1538b87bb43c867fc60f589810b5b86a4d27e
Steven Oldfield <steveold1611@gmail.com>
2019-03-27 07:37:33 -0600

    working now: update IP and find specific OCV then save updated json file. Could use refactor.

--- Lesson5-file-work/Program.cs
+++ Lesson5-file-work/Program.cs
@@ -5,9 +5,7 @@
 // this data will be extracted from json file in the files folder.
 
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.IO;
 
@@ -17,90 +15,63 @@ namespace Lesson5_file_work
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Heaven!");
-            RdFile();
+           RdFile();
         }
 
         public static void RdFile()
         {
-            string filePath1 = "files/lesson-W5.json";
-            // string filePath1 = "D:\\projects\\source\\file\\lesson-W5.json";
-            string filePath2 = "D:\\projects\\source\\file\\lesson-w5-updated.json";
+            //string filePath1 = "files/lesson-W5.json";
+             string filePath1 = "D:\\projects\\source\\file\\lesson-W5.json";
+             string filePath2 = "D:\\projects\\source\\file\\lesson-w5-updated.json";
 
             DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(File.ReadAllText(filePath1));
             DataTable dataTable = dataSet.Tables["results"];
+            int colCount = dataTable.Columns.Count;
 
             foreach (DataRow row in dataTable.Rows)
             {
                 object hn = row["hostName"];
                 string file = (string)hn;
 
+                // If true change IP for this object to '15.13.19.22'
                 if (file.Contains("HP2D0DB0"))
                 {
-                    // If true change IP for this object to '15.13.19.22'
-                    Console.WriteLine(row["ipAddress"]);
+                    Console.WriteLine("Original IPAddress: " + row["ipAddress"]);
                     row["ipAddress"] = "15.13.19.22";
-                    Console.WriteLine(row["ipAddress"]);
-                    // loop through object to display?? or use foreach?  for (i > 0;row.len i++; )
+                    Console.WriteLine("Updated IPAddress: " + row["ipAddress"]);
                 }
                 else
                 {
-                    Console.WriteLine("Losser");
-                }
-                foreach (DataRow row2 in dataTable.Rows)
-                {
-                    // Get the list of the device's ocvStatus and verify if it contains 'GenuineHP'
-                    object ocvStat = row2["ocvStatus"];
-                    string ovc = (string)ocvStat;
-
-
-
-                    if (ovc.Contains("GenuineHP"))
-                    {
-                        Console.WriteLine("WINNER");
-                    }
-
-                    Console.WriteLine("Losser");
-                    Console.ReadKey();
-
+                    // could do a if/for loop to count how many times this is hit and if equals count trigger console message
+                    //  Console.WriteLine("HostName Not found");
                 }
             }
+            // another search through dataset to find the ocvstatus details and print it
+            Console.WriteLine(); //  adds line between sections that are output
+
+            // Get the list of the device's ocvStatus and verify if it contains 'GenuineHP'
+            foreach (DataRow row2 in dataTable.Rows)
+            {
+                object ocvStat = row2["ocvStatus"];
+                string ovc = (string)ocvStat;
+
+                if (ovc.Contains("GenuineHP"))
+                {
+                    for (var i = 0; i < colCount; i++)
+                        Console.WriteLine(dataTable.Columns[i].ColumnName + ": " + row2[i]);
+                }
+                else
+                {
+                    //  same as above: for generating an error type message
+                    //  Console.WriteLine("No Printers have GenuineHP ocvStatus");
+                }
+            }
+            // Save updated json file to new json file (so I can test multiple times)
+            Console.WriteLine();
+            Console.WriteLine("Saving updated file to " + filePath2);
+            string json = JsonConvert.SerializeObject(dataSet, Formatting.Indented);
+            File.WriteAllText(filePath2, json);
+            Console.ReadKey();
         }
     }
 }
-
-/*  Deserialize JSON =======================================
-
-string json = @"{
-  'Name': 'Bad Boys',
-  'ReleaseDate': '1995-4-7T00:00:00',
-  'Genres': [
-    'Action',
-    'Comedy'
-  ]
-}";
-
-Movie m = JsonConvert.DeserializeObject<Movie>(json);
-
-string name = m.Name;
-// Bad Boys
-
-    */
-
- /* Linq to JSON.....==================================
-JArray array = new JArray();
-array.Add("Manual text");
-array.Add(new DateTime(2000, 5, 23));
-
-JObject o = new JObject();
-o["MyArray"] = array;
-
-string json = o.ToString();
-// {
-//   "MyArray": [
-//     "Manual text",
-//     "2000-05-23T00:00:00"
-//   ]
-// }
-
-    */
